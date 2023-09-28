39c026d045db5f2df1f91fced4b7b9971bef3594
LuanSchneider <143182587+LuanSchneider@users.noreply.github.com>
2023-09-27 21:39:37 -0300

    atualização

--- variables.js
+++ variables.js
@@ -1,3 +1,25 @@
-var nome="Luan Schneider";
+//var nome="Luan Schneider";
 
-console.log("meu nome é ",nome);
+/*console.log("meu nome é ",nome);*/
+
+/*function imprimirnome(){
+    var nome ="Luciano da silva Schneider"
+console.log("Meu noome é", nome);
+
+}
+imprimirnome();*/
+/*function imprimiridade(){
+    for(let idade=20 ;idade<50;idade++){
+        console.log("idade",idade);
+    }
+}
+imprimiridade()*/
+
+/*function imprimiridade2(){
+    for(const idade=20 ;idade<50;idade++){
+        console.log("idade",idade);
+    }
+    imprimiridade2();
+    const pessoa={
+
+    }*/
