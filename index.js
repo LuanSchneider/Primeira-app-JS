368cf237334ace90c85037e7764b86367aec3e38
LuanSchneider <143182587+LuanSchneider@users.noreply.github.com>
2023-10-03 08:03:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ console.log("ola mundo");*/
 
 
 
-let=aluno{nome:"luan",email:"luan.pereira142892@gmail.com"idade:20};
+/*let=aluno{nome:"luan",email:"luan.pereira142892@gmail.com"idade:20};
 for(let dados in aluno}{
-  console.log(aluno[dados]);
+  console.log(aluno[dados]);*/
 
+let email_de_cada_aluno={
+"Roberto1223@gmail.com",
+"camilasst@gmail.com",
+  "thales_de_mileto"
+};
+for{let emails in email_de_cada_aluno}{
+  console.log("emails:"+email);
