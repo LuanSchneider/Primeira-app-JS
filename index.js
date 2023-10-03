a9f011518af80a343a140f3610bbece344b065ab
LuanSchneider <143182587+LuanSchneider@users.noreply.github.com>
2023-10-03 07:51:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const lugar="mundo";
+/*const lugar="mundo";
 const saudação = "ola";
 
-console.log("ola mundo");
+console.log("ola mundo");*/
+
+
+
+let=aluno{nome:"luan",email:"luan.pereira142892@gmail.com"idade:20};
+for(let dados in aluno}{
+  console.log(aluno[dados]);
 
